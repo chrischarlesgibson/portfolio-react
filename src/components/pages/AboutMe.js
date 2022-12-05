dc5efa56d63b01f7f988d8b2fe1926b1c337e9ad
Chris Gibson <chrischarlesgibson@gmail.com>
2022-12-04 19:45:50 -0600

    took out skills from about me page and put it into the resume page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -7,34 +7,10 @@ export default function AboutMe() {
   return (
     <div>
       <div className="columns">
-        <div className="column is-half content has-text-centered ">
+        <div className="column is-full content has-text-centered ">
           <h1>About Me</h1>
           <img id="headshot" src={headshot} alt="Chris Gibson headshot" />
           <p>put about me paragraph here</p>
-        </div>
-        <div className="column content has-text-centered ">
-          <h1>Skills</h1>
-          <ul id="skills-list">
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-          </ul>
         </div>
       </div>
       <Footer />
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,25 +5,53 @@ import "../styles/resume.css";
 export default function Resume() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <p className="title">Resume</p>
+      <div className="columns">
+        <div className="column is-half content has-text-centered ">
+          <div className="card">
+            <div className="card-content">
+              <p className="title">Resume</p>
+            </div>
+            <footer className="card-footer">
+              <p className="card-footer-item">
+                <span>
+                  download{" "}
+                  <a href="https://twitter.com/codinghorror/status/506010907021828096">
+                    resume
+                  </a>
+                </span>
+              </p>
+              <p className="card-footer-item">
+                <span>
+                  expand <a href="#">resume</a>
+                </span>
+              </p>
+            </footer>
+          </div>
         </div>
-        <footer className="card-footer">
-          <p className="card-footer-item">
-            <span>
-              download{" "}
-              <a href="https://twitter.com/codinghorror/status/506010907021828096">
-                resume
-              </a>
-            </span>
-          </p>
-          <p className="card-footer-item">
-            <span>
-              expand <a href="#">resume</a>
-            </span>
-          </p>
-        </footer>
+        <div className="column content has-text-centered ">
+          <h1>Skills</h1>
+          <ul id="skills-list">
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+            <li>JavaScript</li>
+          </ul>
+        </div>
       </div>
       <Footer />
     </div>
