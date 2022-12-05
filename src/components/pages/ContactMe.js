3a867f7421c07a900f1df801fdbb49de52c39a42
Chris Gibson <chrischarlesgibson@gmail.com>
2022-12-05 10:58:54 -0600

    added project data and added react hook form validation, but still stroiubleshooting it

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import Footer from "./Footer";
@@ -8,17 +9,33 @@ import { init } from "@emailjs/browser";
 init("REACT_APP_EMAILJS_USER_ID");
 
 export default function ContactMe() {
+  // const emailErrorMessage = document.querySelector("#invalid-email");
+  // const nameErrorMessage = document.querySelector("#invalid-name");
+  // const messageErrorMessage = document.querySelector("#invalid-message");
   const form = useRef();
+  // const emailErrorMessage = useRef();
+  // const nameErrorMessage = useRef();
+  // const messageErrorMessage = useRef();
 
   const [contactFormData, setContactFormData] = useState({
     email: "",
     name: "",
     message: "",
   });
-
+  const { register, handleSubmit, errors } = useForm({
+    defaultValues: contactFormData,
+  });
   const handleChange = (e) =>
     setContactFormData({ ...contactFormData, [e.target.name]: e.target.value });
   console.log("state changed", contactFormData);
+
+  // if (contactFormData.name === "") {
+  //   nameErrorMessage.classList.remove("is-hidden");
+  // } else if (contactFormData.email === "") {
+  //   emailErrorMessage.classList.remove("is-hidden");
+  // } else if (contactFormData.message === "") {
+  //   messageErrorMessage.classList.remove("is-hidden");
+  // }
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -58,8 +75,16 @@ export default function ContactMe() {
                 placeholder="Your Name"
                 onChange={handleChange}
                 value={contactFormData.name}
+                {...register("name", {
+                  required: "name required.",
+                  minLength: {
+                    value: 2,
+                    message: "name should be at-least 2 characters.",
+                  },
+                })}
               />
-              <span className="help is-danger is-hidden">
+              {errors.name && <p className="errorMsg">{errors.name.message}</p>}
+              <span id="invalid-name" className="help is-danger is-hidden">
                 This field is required
               </span>
               <span className="icon is-small is-left">
@@ -76,8 +101,19 @@ export default function ContactMe() {
                 placeholder="Email"
                 onChange={handleChange}
                 value={contactFormData.email}
+                {...register("email", {
+                  required: "Email is required.",
+                  pattern: {
+                    value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                    message: "Email is not valid.",
+                  },
+                })}
               />
-              <span className="help is-danger is-hidden">
+              {errors.email && (
+                <p className="errorMsg">{errors.email.message}</p>
+              )}
+
+              <span id="invalid-email" className="help is-danger is-hidden">
                 Invalid email address
               </span>
               <span className="icon is-small is-left">
@@ -104,8 +140,20 @@ export default function ContactMe() {
                 placeholder="Enter message..."
                 onChange={handleChange}
                 value={contactFormData.message}
-              ></textarea>
-              <span className="help is-danger is-hidden">
+                {...register("message", {
+                  required: "message required.",
+                  minLength: {
+                    value: 5,
+                    message: "message must be at least 5 characters long.",
+                  },
+                })}
+              >
+                {errors.message && (
+                  <p className="errorMsg">{errors.message.message}</p>
+                )}
+              </textarea>
+
+              <span id="invalid-message" className="help is-danger is-hidden">
                 This field is required
               </span>
             </div>
